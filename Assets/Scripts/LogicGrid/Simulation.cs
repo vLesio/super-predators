4aa982e8f1535ac2cdbd9117bf8dfd48adc42c13
Piotr Kubala <pkubala@student.agh.edu.pl>
2024-01-06 00:44:47 +0100

    simulation skeleton

---
+++ Assets/Scripts/LogicGrid/Simulation.cs
@@ -0,0 +1,78 @@
+﻿namespace LogicGrid {
+    public static class Simulation {
+        private static void UpdatePerceptionsForPreys() {
+            
+        }
+
+        private static void UpdateFuzzyCognitiveMapsForPreys() {
+            
+        }
+
+        private static void UpdateActionsAndEnergyForPreys() {
+            
+        }
+        
+        private static void UpdatePerceptionsForPredators() {
+            
+        }
+        
+        private static void UpdateFuzzyCognitiveMapsForPredators() {
+            
+        }
+        
+        private static void UpdateActionsAndEnergyForPredators() {
+            
+        }
+        
+        private static void UpdatePreySpecies() {
+            
+        }
+        
+        private static void UpdatePredatorSpecies() {
+            
+        }
+        
+        private static void UpdateGrass() {
+            
+        }
+        
+        private static void UpdateMeat() {
+            
+        }
+        
+        private static void UpdateAgentsAge() {
+            
+        }
+
+
+        private static void UpdatePreysStep() {
+            UpdatePerceptionsForPreys();
+            UpdateFuzzyCognitiveMapsForPreys();
+            UpdateActionsAndEnergyForPreys();
+        }
+        
+        private static void UpdatePredatorsStep() {
+            UpdatePerceptionsForPredators();
+            UpdateFuzzyCognitiveMapsForPredators();
+            UpdateActionsAndEnergyForPredators();
+        }
+        
+        private static void UpdateSpeciesStep() {
+            UpdatePreySpecies();
+            UpdatePredatorSpecies();
+        }
+        
+        private static void UpdateEnvironmentStep() {
+            UpdateGrass();
+            UpdateMeat();
+        }
+        
+        public static void Update() {
+            UpdatePreysStep();
+            UpdatePredatorsStep();
+            UpdateSpeciesStep();
+            UpdateEnvironmentStep();
+            UpdateAgentsAge();
+        }
+    }
+}
---
+++ Assets/Scripts/LogicGrid/Species.cs
@@ -0,0 +1,4 @@
+﻿namespace LogicGrid {
+    public class Species {
+    }
+}
