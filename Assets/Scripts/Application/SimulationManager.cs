bfcfecef1d8bb6bc1dfca217e71de22497038a18
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2024-01-13 18:25:09 +0100

    MC

---
+++ Assets/Scripts/Application/SimulationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CoinPackage.Debugging;
+using LogicGrid;
+using UnityEngine;
+using Utils.Singleton;
+
+namespace Application {
+    public class SimulationManager : Singleton<SimulationManager> {
+        [SerializeField] private bool isPaused = false;
+
+        private int _steps = 0;
+        private void Update() {
+            if (!isPaused) {
+                Step();
+            }
+        }
+
+        private void Step() {
+            var time = Time.time;
+            Simulation.Update();
+            CDebug.Log($"Simulation step {_steps++} took: {Time.time - time}");
+        }
+    }
+}
