42ecd2f0f54fb1f6a0713140b46e60a8b7558150
empieniazek <empieniazek@student.agh.edu.pl>
2024-01-20 14:59:21 +0100

    Changed

--- Assets/Scripts/Settings/brains/BrainBase.cs
+++ Assets/Scripts/Settings/brains/BrainBase.cs
@@ -18,38 +18,101 @@ namespace Settings.brains
             CreatePredatorMatrix();
         }
 
-        static void CreatePreyMatrix() {
-            var matrix = new double[30, 30];
-            for (var i = 0; i < 30; i++)
+        static void CreatePreyMatrix()
+        {
+            var matrix = new double[,]
             {
-                for (var j = 0; j < 30; j++)
                 {
-                    if (double.IsNaN(matrix[i, j]))
-                    {
-                        matrix[i, j] = 0.0;
-                    }
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 4f, 0f, 0f, 0.1f, 0f, -1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -4f, 0f, 0f, 0f, 0f, 0.5f, -0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.5f, 0f, -0.1f, 0.1f, 0.5f, -0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.4f, 0.2f, -0.2f, -0.7f, 0.7f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.5f, -0.1f, 0.1f, 0.5f, -0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.4f, 0.2f, -0.2f, -0.5f, 0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.4f, 4f, -1.5f, 0f, 0f, -2.2f, 2.2f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, 1.5f, 0.2f, -0.2f, 1.5f, -1.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.2f, 0f, -0.3f, 0.3f, 1.1f, -1.1f, 0f, 0f, 0f, 0f, 0f, 0f, 2.6f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.2f, 0f, 1, -1f, -1.1f, 1.1f, 0f, 0f, 0f, 0f, 0f, 0f, -4f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.4f, 0.4f, 0.5f, -0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 1.5f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.5f, 0.3f, -0.3f, -0.8f, 0.8f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -4f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 3.5f, 0f, -0.8f, -1f, 0.3f, -1f, -1f, -1f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.3f, 0f, 0f, 0f, 0f, 0f, -0.8f, 0f, 2.1f, -0.7f, 0.7f, -0.5f, 4f, -1.8f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.2f, 0f, 0f, 0f, 0f, -0.2f, 0f, 0f, 1.5f, 0.5f, -0.3f, -0.4f, 3f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.1f, 0f, 0f, 0f, -0.1f, 0f, 0.5f, 0.3f, 1.5f, -0.2f, -0.3f, -0.2f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.1f, 0f, 0f, 0f, 0f, -0.5f, -0.3f, -1.2f, 0.2f, 0.3f, 0.2f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.1f, 0f, -0.8f, -0.2f, -2f, 1.5f, 0.8f, 0.7f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.4f, 0f, 1f, 0.2f, 2f, -1.2f, -0.7f, -0.7f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.2f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                },
+                {
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }
-            }
-            matrix[0, 15] = 4; matrix[0, 19] = 0.1; matrix[0, 20] = -1; matrix[0, 21] = 1;
-            matrix[1, 15] = -4; matrix[1, 20] = 0.5; matrix[1, 21] = -0.5;
-            matrix[4, 16] = 0.5; matrix[4, 19] = -0.1; matrix[4, 20] = 0.1; matrix[4, 21] = 0.5; matrix[4, 21] = -0.5;
-            matrix[5, 17] = -0.4; matrix[5, 18] = 0.2; matrix[5, 19] = -0.2; matrix[5, 20] = -0.7; matrix[5, 21] = 0.7;
-            matrix[6, 17] = 0.5; matrix[6, 19] = -0.1; matrix[6, 20] = 0.1; matrix[6, 21] = 0.5; matrix[6, 21] = -0.5;
-            matrix[7, 17] = -4; matrix[7, 18] = 0.2; matrix[7, 19] = -0.2; matrix[7, 20] = -0.5; matrix[7, 21] = 0.5;
-            matrix[8, 15] = 0.4; matrix[8, 16] = 4; matrix[8, 17] = -1.5; matrix[8, 20] = -2.2; matrix[8, 21] = 2.2;
-            matrix[9, 16] = -1; matrix[9, 17] = 1.5; matrix[9, 18] = 0.2; matrix[9, 19] = -0.2; matrix[9, 20] = 1.5; matrix[9, 21] = -1.5;
-            matrix[10, 15] = -0.2; matrix[10, 18] = -0.3; matrix[10, 19] = 0.3; matrix[10, 20] = 1.1; matrix[10, 21] = -1.1; matrix[10, 27] = 2.6;
-            matrix[11, 15] = 0.2; matrix[11, 18] = 1; matrix[11, 19] = -1; matrix[11, 20] = -1.1; matrix[11, 21] = 1.1; matrix[11, 27] = -4;
-            matrix[12, 18] = -0.4; matrix[12, 19] = 0.4; matrix[12, 20] = 0.5; matrix[12, 21] = -0.5; matrix[12, 28] = 1.5;
-            matrix[13, 17] = 0.5; matrix[13, 18] = 0.3; matrix[13, 19] = -0.3; matrix[13, 20] = -8; matrix[13, 21] = 0.8; matrix[13, 28] = -4;
-            matrix[15, 15] = 0.5; matrix[15, 22] = 3.5; matrix[15, 23] = -0.8; matrix[15, 24] = -1; matrix[15, 25] = 0.3; matrix[15, 26] = -1; matrix[15, 27] = -1; matrix[15, 28] = -1;
-            matrix[16, 16] = 0.3; matrix[16, 23] = -0.8; matrix[16, 24] = 2.1; matrix[16, 25] = -0.7; matrix[16, 26] = 0.7; matrix[16, 27] = -0.5; matrix[16, 28] = 4; matrix[16, 29] = -1.8;
-            matrix[17, 17] = 0.2; matrix[17, 23] = -0.2; matrix[17, 24] = 0; matrix[17, 25] = 1.5; matrix[17, 26] = 0.5; matrix[17, 27] = -0.3; matrix[17, 28] = -0.4; matrix[17, 29] = 3;
-            matrix[18, 18] = 0.1; matrix[18, 23] = -0.1; matrix[18, 24] = 0.5; matrix[18, 25] = 0.3; matrix[18, 26] = 1.5; matrix[18, 27] = -2; matrix[18, 28] = -0.3; matrix[18, 29] = -2;
-            matrix[19, 19] = 0.1; matrix[19, 24] = -0.5; matrix[19, 25] = -0.3; matrix[19, 26] = -1.2; matrix[19, 27] = 0.2; matrix[19, 28] = 0.3; matrix[19, 29] = 0.2;
-            matrix[20, 23] = -0.1; matrix[20, 24] = -0.8; matrix[20, 25] = -0.2; matrix[20, 26] = -2; matrix[20, 27] = 1.5; matrix[20, 28] = 0.8; matrix[20, 29] = 0.7;
-            matrix[21, 23] = 0.4; matrix[21, 24] = 1; matrix[21, 25] = 0.2; matrix[21, 26] = 2; matrix[21, 27] = -1.2; matrix[21, 28] = -0.7; matrix[21, 29] = -7;
-            matrix[27, 26] = 0.2;
+            };
 
             PreyMatrix = Matrix<double>.Build.Dense(30, 30, matrix.Cast<double>().ToArray());
         }
@@ -57,95 +120,65 @@ namespace Settings.brains
         static void CreatePredatorMatrix() {
             var matrix = new double[,] {
                 {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.7f, 0f, 0f, 0f, -0.1f, 0f, 0.5f, -0.5f,
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.7f, 0f, 0f, 0f, -0.1f, 0f, 0.5f, -0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.5f, 0f, 0.7f, 0.1f, 0.4f, -0.4f, -0.5f,
-                    0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.5f, 0f, 0.7f, 0.1f, 0.4f, -0.4f, -0.5f, 0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.5f, 0f, 0.7f, 0f, -0.1f, 0.1f, 0.5f,
-                    -0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.5f, 0f, 0.7f, 0f, -0.1f, 0.1f, 0.5f, -0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.8f, 0f, -0.2f, 0.1f, 0.2f, -0.2f, -0.6f,
-                    0.6f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.8f, 0f, -0.2f, 0.1f, 0.2f, -0.2f, -0.6f, 0.6f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.7f, 0f, 0f, 0.4f, -0.4f, 0f,
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.7f, 0f, 0f, 0.4f, -0.4f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.5f, 0.3f, -0.3f, -4f, 0.4f,
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.5f, 0.3f, -0.3f, -4f, 0.4f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 3.5f, 0f, 5f, -1.2f, 0f, 0.2f, -1.5f, 1.5f,
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 3.5f, 0f, 5f, -1.2f, 0f, 0.2f, -1.5f, 1.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -2f, 0f, -0.3f, 1.4f, 0.3f, -0.3f, 1f, -1f,
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -2f, 0f, -0.3f, 1.4f, 0.3f, -0.3f, 1f, -1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -1.5f, 0f, 0.3f, -0.2f, -0.3f, 0.3f, 1f,
-                    -1f, 0f, 0f, 0f, 0f, 0f, 0f, 4f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -1.5f, 0f, 0.3f, -0.2f, -0.3f, 0.3f, 1f, -1f, 0f, 0f, 0f, 0f, 0f, 0f, 4f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 1.7f, 0f, 0f, 0.2f, 1f, -1f, -1f, 1f, 0f,
-                    0f, 0f, 0f, 0f, 0f, -5f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 1.7f, 0f, 0f, 0.2f, 1f, -1f, -1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -5f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.3f, 0f, 0f, 0f, 0.4f, 0.4f, 0.8f, -0.8f,
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 2f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.3f, 0f, 0f, 0f, 0.4f, 0.4f, 0.8f, -0.8f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 2f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.3f, 0f, 0f, 0.5f, 0.3f, -0.3f, -8f, 0.8f,
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, -5f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.3f, 0f, 0f, 0.5f, 0.3f, -0.3f, -8f, 0.8f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -5f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.2f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 1.5f,
-                    -0.2f, -0.4f, 0.3f, -0.4f, 0f, -0.4f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.2f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 1.5f, -0.2f, -0.4f, 0.3f, -0.4f, 0f, -0.4f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.3f, 0f, 0f, 0f, 0f, 0f, 0f, 1.5f,
-                    2.5f, -1.2f, 0.3f, -0.4f, 3.5f, -0.8f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.3f, 0f, 0f, 0f, 0f, 0f, 0f, 1.5f, 2.5f, -1.2f, 0.3f, -0.4f, 3.5f, -0.8f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.2f, 0f, 0f, 0f, 0f, 0f, -0.8f,
-                    -0.8f, 1.5f, 0.3f, -0.5f, -0.6f, 3f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.2f, 0f, 0f, 0f, 0f, 0f, -0.8f, -0.8f, 1.5f, 0.3f, -0.5f, -0.6f, 3f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.1f, 0f, 0f, 0f, 0f, 0.3f,
-                    0.3f, 0.3f, 1.5f, -0.4f, -0.3f, -0.2f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.1f, 0f, 0f, 0f, 0f, 0.3f, 0.3f, 0.3f, 1.5f, -0.4f, -0.3f, -0.2f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.1f, 0f, 0f, 0f, -0.3f,
-                    -0.3f, -0.3f, -1.5f, 0.4f, 0.3f, 0.2f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.1f, 0f, 0f, 0f, -0.3f, -0.3f, -0.3f, -1.5f, 0.4f, 0.3f, 0.2f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.8f,
-                    -0.8f, -0.2f, -1.8f, 1f, 0.8f, 0.8f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, -0.8f, -0.8f, -0.2f, -1.8f, 1f, 0.8f, 0.8f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 1f,
-                    0.8f, 0.2f, 2f, -1f, -0.6f, -0.8f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 1f, 0.8f, 0.2f, 2f, -1f, -0.6f, -0.8f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0.2f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0.2f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }, {
-                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f,
-                    0f, 0f, 0f, 0f, 0f
+                    0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f
                 }
             };
             // var matrix = new double[31, 31];
